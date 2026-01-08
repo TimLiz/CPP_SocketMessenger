f8fe45b28ffae1ea6a0045727743d9e0a738369c
TimLiz <@>
2026-01-08 14:10:51 +0300

    Switched to non owning pointer since using unique_ptr was incorrect there

--- Common/src/Network/PacketView.cpp
+++ Common/src/Network/PacketView.cpp
@@ -2,13 +2,13 @@
 #include "fbs/Base_generated.h"
 
 namespace Network {
-    PacketView::PacketView(const std::span<std::byte>& dataView):buffer(dataView) {}
+    PacketView::PacketView(const std::span<std::byte>& dataView): buffer(dataView) {}
 
-    std::unique_ptr<const Packets::Base> PacketView::GetParsedView() {
+    const Packets::Base* PacketView::GetParsedView() {
         if (!verified) {
             throw std::logic_error("Tried to parse non verified packet");
         }
-        return std::unique_ptr<const Packets::Base>(Packets::GetBase(this->buffer.data()));
+        return Packets::GetBase(this->buffer.data());
     }
 
     std::span<const std::byte> PacketView::getBufferView() {
@@ -16,7 +16,8 @@ namespace Network {
     }
 
     bool PacketView::verify() {
-        auto verifier = flatbuffers::Verifier(reinterpret_cast<unsigned char*>(this->buffer.data()), this->buffer.size());
+        auto verifier = flatbuffers::Verifier(reinterpret_cast<unsigned char*>(this->buffer.data()),
+                                              this->buffer.size());
         verified = Packets::VerifyBaseBuffer(verifier);
         return verified;
     }
